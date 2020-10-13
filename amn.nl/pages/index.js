e4a195a690225a91e9f0f87916174bbbd1dd5e88
Bogdan Craciun <bogdan@amn.nl>
2020-10-13 17:57:43 +0300

    re-split

--- amn.nl/pages/index.js
+++ amn.nl/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Bla from '../../components/bla';
 
 export default function Home() {
 	return (
@@ -11,6 +12,7 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>Welcome to amn.NL</h1>
+				<Bla />
 
 				<p className={styles.description}>
 					Get started by editing{' '}
---
+++ components/bla.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Bla = () => <div>BLA</div>;
+
+export default Bla;
